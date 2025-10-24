caa357b08909031fc23963e2df139fd9d096ee89
moSa963 <mo.sa963@hotmail.com>
2025-10-24 18:57:45 +0300

    Adding flat button widget

---
+++ lib/widgets/flat_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FlatButton extends StatelessWidget {
+  const FlatButton({super.key, required this.onPressed, required this.child});
+
+  final void Function()? onPressed;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        side: BorderSide(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withAlpha(100),
+        ),
+      ),
+      child: Text("data"),
+    );
+  }
+}
