b4a3e68bc7d5ac6158f07c268fe36acfd88c0764
moSa963 <mo.sa963@hotmail.com>
2025-10-24 18:59:59 +0300

    Adding button list

---
+++ lib/widgets/buttons_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:push_up_flutter/widgets/flat_button.dart';
+
+class ButtonList extends StatelessWidget {
+  const ButtonList({super.key, required this.title, required this.list});
+  final String title;
+  final List<FlatButton> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Theme.of(context).splashColor.withAlpha(10),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: 10,
+        children: [
+          Text(title),
+          Wrap(spacing: 5, children: list),
+        ],
+      ),
+    );
+  }
+}
