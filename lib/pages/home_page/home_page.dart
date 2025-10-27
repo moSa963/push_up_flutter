16d7c881cfe1a1f9a012f86eac579f62b4f3b988
moSa963 <mo.sa963@hotmail.com>
2025-10-27 20:14:24 +0300

    Moving home page into a home_page folder

---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:push_up_flutter/pages/home_page/routine_buttons.dart';
+import 'package:push_up_flutter/widgets/ListView/list_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(label: "home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "list", icon: Icon(Icons.list)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(10),
+        child: ListView(
+          children: [
+            RoutineButtons(),
+            SizedBox(height: 10),
+            MainListView(children: [
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
