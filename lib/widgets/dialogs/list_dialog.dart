36444fe4ff5d750cd53dfc6b3902f15106932b1a
moSa963 <mo.sa963@hotmail.com>
2025-11-06 20:17:24 +0300

    Updating list dialog style and adding Material class

--- lib/widgets/dialogs/list_dialog.dart
+++ lib/widgets/dialogs/list_dialog.dart
@@ -8,43 +8,41 @@ class ListDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FractionallySizedBox(
-        widthFactor: 0.95,
-        heightFactor: 0.95,
-        child: Container(
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(25)),
-            color: Theme.of(context).cardColor,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-                child: Row(
-                  children: [
-                    DialogButton(
-                      onPressed: () => _handleClose(context),
-                      color: Colors.redAccent,
-                      child: Icon(Icons.close),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: actions ?? [],
+    return Material(
+      color: Color.fromARGB(100, 0, 0, 0),
+      child: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.90,
+          heightFactor: 0.90,
+          child: Container(
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+              color: Theme.of(context).colorScheme.surfaceContainerHigh,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                  child: Row(
+                    children: [
+                      DialogButton(
+                        onPressed: () => _handleClose(context),
+                        color: Colors.redAccent,
+                        child: Icon(Icons.close),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: actions ?? [],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsetsGeometry.all(5),
-                  child: child,
-                ),
-              ),
-            ],
+                Expanded(child: child),
+              ],
+            ),
           ),
         ),
       ),
