c6d8e630a60f1709a56739a10138175c12c5cc9e
moSa963 <mo.sa963@hotmail.com>
2025-11-14 18:36:09 +0300

    Adding counter widget

---
+++ lib/widgets/counter.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Counter<T extends num> extends StatelessWidget {
+  const Counter({
+    super.key,
+    required this.value,
+    required this.onChange,
+    this.max = 100,
+    this.min = 0,
+    this.step = 1,
+  });
+
+  final T value;
+  final num max;
+  final num min;
+  final num step;
+  final void Function(T val) onChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () => _handleChange(-step as T),
+          icon: const Icon(Icons.remove),
+        ),
+        Text(value.toString()),
+        IconButton(
+          onPressed: () => _handleChange(_toT(step)),
+          icon: const Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+
+  void _handleChange(T delta) {
+    final num newValue = value + delta;
+
+    if (newValue <= max && newValue >= min) {
+      onChange(_toT(newValue));
+    } else if (delta > 0 && value != max) {
+      onChange(_toT(max));
+    } else if (delta < 0 && value != min) {
+      onChange(_toT(min));
+    }
+  }
+
+  T _toT(num n) {
+    if (value is int) {
+      return n.toInt() as T;
+    } else {
+      final rounded = double.parse(n.toStringAsFixed(2));
+      return rounded as T;
+    }
+  }
+}
