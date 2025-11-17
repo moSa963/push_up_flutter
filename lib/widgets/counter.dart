258fae676c7dd59cd24b08f3c721c4711949203a
moSa963 <mo.sa963@hotmail.com>
2025-11-17 18:51:30 +0300

    Adding unit prop to Coounter

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -8,23 +8,27 @@ class Counter<T extends num> extends StatelessWidget {
     this.max = 100,
     this.min = 0,
     this.step = 1,
+    this.unit = "",
   });
 
   final T value;
   final num max;
   final num min;
   final num step;
+  final String unit;
   final void Function(T val) onChange;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      spacing: 3,
       children: [
         IconButton(
           onPressed: () => _handleChange(-step as T),
           icon: const Icon(Icons.remove),
         ),
         Text(value.toString()),
+        Text(unit),
         IconButton(
           onPressed: () => _handleChange(_toT(step)),
           icon: const Icon(Icons.add),
