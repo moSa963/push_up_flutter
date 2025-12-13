a84db731e9b8fdc63010c5076da6cd08c4f52689
moSa963 <mo.sa963@hotmail.com>
2025-12-13 15:05:50 +0300

    Adding routine dialog

--- lib/widgets/dialogs/create_routine_dialog.dart
+++ lib/widgets/dialogs/create_routine_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:push_up_flutter/widgets/dialogs/list_dialog.dart';
+import 'package:push_up_flutter/models/routine_model.dart';
+import 'package:push_up_flutter/widgets/dialogs/dialog_button.dart';
+import 'package:push_up_flutter/widgets/dialogs/navigable_dialog.dart';
+import 'package:push_up_flutter/widgets/exercise_form.dart';
 
 class CreateRoutineDialogs extends StatefulWidget {
   const CreateRoutineDialogs({super.key});
@@ -9,25 +12,52 @@ class CreateRoutineDialogs extends StatefulWidget {
 }
 
 class _CreateRoutineDialogsState extends State<CreateRoutineDialogs> {
+  final RoutineModel _routine = RoutineModel(name: "");
+  final _key = ExerciseForm.globalKey;
+
   @override
   Widget build(BuildContext context) {
-    return ListDialog(
-      child: ListView(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              labelText: 'Title',
-              border: InputBorder.none,
-              fillColor: Theme.of(context).colorScheme.surfaceContainerHighest,
+    return NavigableDialog(
+      builder: (push, pop) {
+        return ListView(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'next one',
+                labelStyle: TextStyle(color: Theme.of(context).hintColor),
+                border: InputBorder.none,
+                fillColor: Theme.of(
+                  context,
+                ).colorScheme.surfaceContainerHighest,
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _routine.name = value;
+                });
+              },
             ),
 
-            onChanged: (text) {
-              print(text);
-            },
-          ),
-        ],
-      ),
+            IconButton(
+              onPressed: () => push(ExerciseForm(key: _key), [
+                DialogButton(
+                  onPressed: () {
+                    final ex = _key.currentState?.exercise;
+
+                    if (ex != null) {
+                      _routine.exercises.add(ex);
+                    }
+
+                    pop();
+                  },
+                  child: Icon(Icons.check),
+                ),
+              ]),
+              icon: Icon(Icons.add),
+            ),
+          ],
+        );
+      },
     );
   }
 }
